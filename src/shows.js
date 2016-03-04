b8def7b79d7136867340fdc3ab916da6554dc631
thread <thethread@gmail.com>
2016-03-04 18:43:43 -0500

    wip

--- src/shows.js
+++ src/shows.js
@@ -1,27 +1,34 @@
 import {inject} from "aurelia-framework";
 import {State} from "./state";
+// import {Router} from "aurelia-router";
 
+// @inject(State, Router)
 @inject(State)
 export class Shows {
 
+  // constructor(state, router) {
+  //   this.state  = state;
+  //   this.router = router;
   constructor(state) {
-    this.state = state;
+    this.state  = state;
+
+    if (this.state.shows.length === 0) {
+      alert("You has no shows");
+      this.router.navigate("login");
+    }
   }
 
   configureRouter(config, router) {
     this.router = router;
 
-    var gotDefault = false;
-
     config.map(this.state.shows.map((show) => {
       return {
-        route:    (gotDefault ? [] : [""]).concat([show.slug]),
+        route:    show.slug,
         name:     show.slug,
         title:    show.name,
         moduleId: "./show",
         nav:      true
       };
-      gotDefault = true;
     }));
   }
 }
