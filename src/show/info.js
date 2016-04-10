048f99d7f1e41770eca31674908ba7be1fab8870
djthread <thethread@gmail.com>
2016-04-09 22:19:17 -0400

    schedule is go

--- src/show/info.js
+++ src/show/info.js
@@ -8,11 +8,31 @@ export class Info {
     this.state = state;
   }
 
-  // activate() {
-  //   return new Promise((accept, reject) => {
-  //     this.state.getInfo(() => {
-  //       accept()
-  //     });
-  //   }.bind(this));
-  // }
+  attached() {
+    console.log('attached show', this.show);
+    this.show = this.state.show;
+  }
+
+  submit() {
+    var args, state = this.state;
+
+    args = { show: {
+      id:         this.show.id,
+      tiny_info:  this.show.tiny_info,
+      short_info: this.show.short_info,
+      full_info:  this.show.full_info
+    }};
+
+    state.push("save_info", args, () => {
+      console.log("woop", arguments);
+      state.getShow(state.show.id, () => {
+        state.flashMsg("Info saved!");
+        // this.router.navigateToRoute("episodes");
+      });
+    }.bind(this), (ret) => {
+      this.errors = ret.errors;
+      console.log('uhh', arguments);
+      console.log('ERRORS', this.errors);
+    }.bind(this));
+  }
 }
